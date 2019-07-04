e993fb58a3e1c858f1fdfbeac4517a9756da980a
ziv <1377490586@qq.com>
2019-07-04 16:45:35 +0800

    feign服务调用

---
+++ kirin-data-feign/src/main/java/com/ziv/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.ziv.controller;
+
+import com.ziv.entity.Product;
+import com.ziv.service.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Resource
+    private ProductService productService;
+
+    @RequestMapping("/products")
+    public Object products() {
+        List<Product> ps = productService.listProducts();
+        return ps;
+    }
+
+}
