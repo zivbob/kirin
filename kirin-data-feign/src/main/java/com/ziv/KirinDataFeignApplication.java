309e7a7a6f2aedddb36ce9192e4500d38c299e0a
ziv <1377490586@qq.com>
2019-07-08 15:46:48 +0800

    配置中心搭建

--- kirin-data-feign/src/main/java/com/ziv/KirinDataFeignApplication.java
+++ kirin-data-feign/src/main/java/com/ziv/KirinDataFeignApplication.java
@@ -4,6 +4,7 @@ import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@RefreshScope
 public class KirinDataFeignApplication {
 
     public static void main(String[] args) {
--- kirin-data-feign/src/main/java/com/ziv/controller/ProductController.java
+++ kirin-data-feign/src/main/java/com/ziv/controller/ProductController.java
@@ -2,14 +2,19 @@ package com.ziv.controller;
 
 import com.ziv.entity.Product;
 import com.ziv.service.ProductService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@RefreshScope
 public class ProductController {
+
+    @Value("${mina.appId}")
+    private String appId;
 
     @Resource
     private ProductService productService;
@@ -18,6 +23,11 @@ public class ProductController {
     public Object products() {
         List<Product> ps = productService.listProducts();
         return ps;
+    }
+
+    @RequestMapping("/config")
+    public Object config() {
+        return appId;
     }
 
 }
