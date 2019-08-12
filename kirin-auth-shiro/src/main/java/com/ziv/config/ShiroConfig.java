6e44acbddc4387e41ac12fa6a52bd02132d64609
ziv <1377490586@qq.com>
2019-08-12 17:12:12 +0800

    shiro权限认证

--- kirin-auth-shiro/src/main/java/com/ziv/config/ShiroConfig.java
+++ kirin-auth-shiro/src/main/java/com/ziv/config/ShiroConfig.java
@@ -4,10 +4,12 @@ import com.ziv.auth.AuthFilter;
 import com.ziv.auth.AuthRealm;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,9 +28,8 @@ public class ShiroConfig {
     @Bean
     public SessionManager sessionManager() {
         DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-        sessionManager.setSessionValidationSchedulerEnabled(true);
-        //
-        sessionManager.setSessionIdCookieEnabled(false);
+        // 禁用session缓存
+        // sessionManager.setSessionIdCookieEnabled(false);
         return sessionManager;
     }
 
@@ -51,6 +52,20 @@ public class ShiroConfig {
         filterMap.put("/**", "oauth2");
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterMap);
         return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
+        proxyCreator.setProxyTargetClass(true);
+        return proxyCreator;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+        advisor.setSecurityManager(securityManager);
+        return advisor;
     }
 
 }
--- kirin-auth-shiro/src/main/java/com/ziv/controller/TestController.java
+++ kirin-auth-shiro/src/main/java/com/ziv/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.ziv.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +15,12 @@ public class TestController {
     @GetMapping
     public JsonResult get() {
         return JsonResult.success("you got it");
+    }
+
+    @RequiresPermissions(value = {"user:teach"})
+    @GetMapping("per")
+    public JsonResult getInfo() {
+        Subject subject = SecurityUtils.getSubject();
+        return JsonResult.success(subject.getPrincipal());
     }
 }
